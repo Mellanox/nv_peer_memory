aafb91d9a4b595c8b56e047c31408b9155e9372d
Alaa Hleihel <alaa@mellanox.com>
2016-08-29 14:26:16 +0300

    build_release.sh: script for building src.rpm
    By default the script will clone master branch of the Github tree,
    and builds src.rpm and source tarball.
    
    Use GIT_URL and GIT_BRANCH env. variables to build from different tree
    location/branch.
    
    Signed-off-by: Alaa Hleihel <alaa@mellanox.com>

--- build_release.sh
+++ build_release.sh
@@ -1,32 +1,74 @@
 #!/bin/bash
-set -e
-SCRIPTPATH=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
-force=${force:-0}
+# vim: ts=4 sw=4 expandtab
+#
+# Author: Alaa Hleihel <alaa@mellanox.com>
+#
 
+GIT_URL=${GIT_URL:-"https://github.com/Mellanox/nv_peer_memory.git"}
 
-echo "Building source packages..."
+GIT_BRANCH=${GIT_BRANCH:-"master"}
+ex()
+{
+    if ! eval "$@"; then
+        echo "Failed to execute: $@" >&2
+        exit 1
+    fi
+}
+
+tmpdir=`mktemp -d /tmp/nv.XXXXXX`
+if [ ! -d "$tmpdir" ]; then
+    echo "Failed to create a temp directory!" >&2
+    exit 1
+fi
+echo "Working in $tmpdir ..."
+cd $tmpdir
+
+#clone
+echo "Cloning from $GIT_URL ..."
+ex git clone $GIT_URL >/dev/null
+dirname=`ls -1`
+cd $dirname
+echo "Checking out branch: $GIT_BRANCH ..."
+ex git checkout $GIT_BRANCH >/dev/null
 VERSION=`grep Version: *.spec | cut -d : -f 2 | sed -e 's@\s@@g'`
 RELEASE=`grep "define _release" *.spec | cut -d" " -f"4"| sed -r -e 's/}//'`
-
-
-rm -rf release
-mkdir release
-cd release
-mkdir nvidia_peer_memory-$VERSION-$RELEASE
-
-cp ../README.txt nvidia_peer_memory-$VERSION-$RELEASE/
-
-for file in $($SCRIPTPATH/gen_nvidia_plugin_srpm.sh 2>/dev/null | grep Built: | sed -r -e 's/.*\s//')
-do
-	mv $file nvidia_peer_memory-$VERSION-$RELEASE/
-done
-
-echo building package tarball...
-tar czf nvidia_peer_memory-$VERSION-$RELEASE.tar.gz nvidia_peer_memory-$VERSION-$RELEASE
-rm -rf nvidia_peer_memory-$VERSION-$RELEASE
-
-if [ $force -eq 0 ] && [ -f "/mswg/release/nvidia_peer_memory/nvidia_peer_memory-$VERSION-$RELEASE.tar.gz" ]; then
-	echo "Error: /mswg/release/nvidia_peer_memory/nvidia_peer_memory-$VERSION-$RELEASE.tar.gz already exists!"
-	exit 1
+if [ "X$VERSION" == "X" ] || [ "X$RELEASE" == "X" ]; then
+    echo "Failed to get version numbers!" >&2
+    exit 1
 fi
-cp nvidia_peer_memory-$VERSION-$RELEASE.tar.gz /mswg/release/nvidia_peer_memory/
+
+cd $tmpdir
+ex mv $dirname nvidia_peer_memory-$VERSION
+ex tar czf nvidia_peer_memory-$VERSION.tar.gz nvidia_peer_memory-$VERSION --exclude=.* --exclude=build_release.sh
+
+echo
+echo "Building source rpm for nvidia_peer_memory..."
+mkdir -p $tmpdir/topdir/{SRPMS,RPMS,SPECS,BUILD}
+ex "rpmbuild -ts --nodeps --define '_topdir $tmpdir/topdir' --define 'dist %{nil}' --define '_source_filedigest_algorithm md5' --define '_binary_filedigest_algorithm md5' nvidia_peer_memory-$VERSION.tar.gz >/dev/null"
+srpm=`ls -1 $tmpdir/topdir/SRPMS/`
+mv $tmpdir/topdir/SRPMS/$srpm /tmp
+
+echo "Building debian tarball for nvidia-peer-memory..."
+ex mv nvidia_peer_memory-$VERSION nvidia-peer-memory-$VERSION
+# update version in changelog
+sed -i -r "0,/^(.*) \(([a-zA-Z0-9.-]+)\) (.*)/s//\1 \(${VERSION}-${RELEASE}\) \3/" nvidia-peer-memory-${VERSION}/debian/changelog
+ex tar czf nvidia-peer-memory_$VERSION.orig.tar.gz nvidia-peer-memory-$VERSION --exclude=.* --exclude=build_release.sh
+ex mv nvidia-peer-memory_$VERSION.orig.tar.gz /tmp
+
+/bin/rm -rf $tmpdir
+
+echo ""
+echo Built: /tmp/$srpm
+echo Built: /tmp/nvidia-peer-memory_$VERSION.orig.tar.gz
+echo ""
+echo "To install run on RPM based OS:"
+echo "    # rpmbuild --rebuild /tmp/$srpm"
+echo "    # rpm -ivh <path to generated binary rpm file>" 
+echo ""
+echo "To install on DEB based OS:"
+echo "    # cd /tmp"
+echo "    # tar xzf /tmp/nvidia-peer-memory_$VERSION.orig.tar.gz"
+echo "    # cd nvidia-peer-memory-$VERSION"
+echo "    # dpkg-buildpackage -us -uc"
+echo "    # dpkg -i <path to generated deb files>"
+echo ""
